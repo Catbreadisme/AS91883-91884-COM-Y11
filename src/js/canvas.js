d98c1901b6f1ad28966e648611caebaab65c3051
bowdeco <bowdeco@whs.school.nz>
2022-05-17 14:47:04 +1200

    Updated comments

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,13 +1,15 @@
 // This script controls the canvas
 
-
+// Size of the canvas
 const WIDTH = 800;
 const HEIGHT = 800;
 
+// Get the canvas element from html
 const doc = document.getElementById("mainCanvas")
 
+// Set the canvas to be the same size as the WIDTH and HEIGHT consts
 console.log(doc)
-doc.setAttribute("width", WIDTH);
+doc.setAttribute("width", WIDTH); 
 doc.setAttribute("height", HEIGHT);
 
 
@@ -19,8 +21,9 @@ let yPosition = 0;
 let xSpeed = 1;
 let ySpeed = 1;
 
-
+// Start the canvas
 window.onload = canvasStart
+
 
 function canvasStart() {
     ctx = doc.getContext("2d") // Get the canvas element
@@ -28,10 +31,11 @@ function canvasStart() {
     fps = setInterval(canvasUpdate, 0.6) //The amount of times the canvas is called in a second. (Currently 60Fps)
 }
 
+// Updates 60 times per seconds, see fps
 function canvasUpdate() {
 
-    ctx.fillStyle ="black"
-    ctx.fillRect(0, 0, WIDTH, HEIGHT)
+    ctx.fillStyle ="black"  // Set colour to black
+    ctx.fillRect(0, 0, WIDTH, HEIGHT)   // Clear the canvas every frame
 
-    moveTest()
+    moveTest()  // Run the movetest function, see input.js
     }
