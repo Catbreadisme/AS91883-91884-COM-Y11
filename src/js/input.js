73de3d2ba9b667f371c7c0713d1f186b480e6dbe
bowdeco <bowdeco@whs.school.nz>
2022-05-31 15:20:02 +1200

    Cleaned up code and added comments

--- src/js/input.js
+++ src/js/input.js
@@ -12,37 +12,31 @@ let ySize = 100
         
     }   
 
+    // Offsets Object
     let offsets = {} // Movement and dragging offsets
 
-    let mousePressed = false; // Variable that is used to show if mouse if down
-    let mouseMoveActive = false;   
-    let objectSelected = false;
-    let id;
+    // Object Movement Variables
+    let mousePressed = false;       // Variable that is used to show if mouse if down
+    let mouseMoveActive = false;    // Variable that shows if the object can be moved
+    let objectSelected = false;     // Variable that shows if an object is selected
+    let id;                         // Stores which object is currently selected
     
     
-
+    // Called when the mouse is clicked
     function onDown(mouseEvent) {
         mousePressed = true // Mouse is down
         
         for (let i = 0; i < plantPots.length; i++){
-            //console.log(i)
-            //mouseOffsets(mouseEvent, i, {once: true})
             
             if(objectDetectedBool(mouseEvent, i, {once: true})){
-                id = i;
-                console.log(id)
-                objectSelected = true
+                id = i; // Sets the id to the currently selected object
+                console.log(id) // Logs the id, (Currently just used for testing)
+                objectSelected = true // Sets to true to show that an object has been selected
 
-                offsets.x = mouseEvent.offsetX - plantPots[id].xPosition
-                offsets.y = mouseEvent.offsetY - plantPots[id].yPosition
+                offsets.x = mouseEvent.offsetX - plantPots[id].xPosition // Creates an offset on the x, This removes weird snapping
+                offsets.y = mouseEvent.offsetY - plantPots[id].yPosition // Creates an offset on the y, This removes weird snapping
 
-                mouseMoveActive = true
-                /* if(collisionDetectedBool(mouseEvent, id)) {
-                    mouseMoveActive = true // Square can now move
-                    
-                    console.log(id)
-                    
-                } */
+                mouseMoveActive = true // Sets to true to show that the object can now move
             }
         }
         
@@ -50,57 +44,31 @@ let ySize = 100
            
     }
 
+    // Called when the mouse moves
     function mouseMovement(mouseEvent) {
-        //console.log(plantPots.length)
         if (mousePressed && mouseMoveActive && objectSelected){
         plantPots[id].xPosition = mouseEvent.offsetX - offsets.x; // Moves the pot on the x axis
         plantPots[id].yPosition = mouseEvent.offsetY - offsets.y; // Moves the pot on the y axis
         }
-        //console.log("x " + mouseX, "y " + mouseY)
     }
     
+    // Called when the mouse is released
     function onUp(){
-        /* window.removeEventListener("mouseup", onUp)
-        window.removeEventListener("mousemove", mouseMovement) */
-        mousePressed = false; // Mouse 
-        mouseMoveActive = false;
-        objectSelected = false;
+        mousePressed = false; // Mouse is released
+        mouseMoveActive = false; // Object no longer moves
+        objectSelected = false; // Object is no longer selected
     }
 
-    /* function collisionDetectedOG(mouseEvent){
-        if(mouseEvent.offsetX >= xPosition && 
-            mouseEvent.offsetY >= yPosition && 
-            mouseEvent.offsetX <= xPosition + xSize &&
-            mouseEvent.offsetY <= yPosition + ySize
-        ){
-            collision = true;
-        }else{
-            collision = false;
-        }
-    } */
-    function collisionDetectedBool(mouseEvent){
-        //console.log(i)
-        if(mouseEvent.offsetX >= plantPots[id].xPosition && 
-            mouseEvent.offsetY >= plantPots[id].yPosition && 
-            mouseEvent.offsetX <= plantPots[id].xPosition + xSize &&
-            mouseEvent.offsetY <= plantPots[id].yPosition + ySize
-            && plantPots[id].isDraggable)
-        {
-            return true;
-        }else{
-            return false;
-        }
-    }
-
+    // Called when mouse is down, and checks for object collision
     function objectDetectedBool(mouseEvent, i) {
         if(mouseEvent.offsetX >= plantPots[i].xPosition && 
             mouseEvent.offsetY >= plantPots[i].yPosition && 
             mouseEvent.offsetX <= plantPots[i].xPosition + xSize &&
-            mouseEvent.offsetY <= plantPots[i].yPosition + ySize)
+            mouseEvent.offsetY <= plantPots[i].yPosition + ySize) // Collision code for the objects
             {
-                return true;
+                return true; // return true if collision
             }else{
-                return false;
+                return false; // return false if no collision
             }
     }
 
